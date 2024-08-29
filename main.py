f765dd6ea742c48f0ae44f2f932d85077b524d6e
Minh Nguyen <minhnq.dev@gmail.com>
2024-08-29 10:15:03 +0700

    feat: test cache func

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 # encoding: utf-8
+import random
+import functools
+
+
+@functools.lru_cache(maxsize=2)
+def test_func1(a: int, b: int) -> int:
+    # res = int(random.random()) + a + b
+    res = a + b
+    print("def getting val==>", random.random(), res)
+    return res
+
 if __name__ == '__main__':
     print('Hello, World!')
-
+    print(test_func1(1, 2))
+    print(test_func1(2, 3))
+    print(test_func1(1, 2))
+    print(test_func1(6, 7))
+    print(test_func1(1, 2))
+    print(test_func1(2, 3))
+    print(test_func1.cache_info())
