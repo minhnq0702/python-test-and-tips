ef1f62bfecbc34875b5258a9bb5553bace9cbd46
minhnq0702 <minhnq.0702@gmail.com>
2024-09-07 16:33:49 +0700

    schedule a job

--- dagster-tutorial/tutorial/definitions.py
+++ dagster-tutorial/tutorial/definitions.py
@@ -1,9 +1,17 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, ScheduleDefinition, load_assets_from_modules, define_asset_job, AssetSelection
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+hackernews_top_words_job = define_asset_job("job_hacknews_top_words", selection=AssetSelection.all())
+
+hackernews_to_words_schedule = ScheduleDefinition(
+    job=hackernews_top_words_job,
+    cron_schedule="*/1 * * * *"
+)
+
 defs = Definitions(
     assets=all_assets,
+    schedules=[hackernews_to_words_schedule],
 )
