c12a2c99bda7a1e50c3970ccc2d9a67c95b7f55f
minhnq0702 <minhnq.0702@gmail.com>
2024-09-08 14:37:07 +0700

    feat: add manhattan trips status map

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -1,7 +1,11 @@
 import os
+import base64
 import duckdb
 import pandas as pd
 import geopandas as gpd
+import plotly.express as px
+import plotly.graph_objects
+import plotly.io as pio
 
 from dagster import asset, MaterializeResult, MetadataValue, AssetExecutionContext
 
@@ -9,7 +13,7 @@ from . import constants
 
 
 @asset(deps=["nyc_tax_trips", "nyc_tax_zones"])
-def nyc_manhattan_stats(context: AssetExecutionContext) -> MaterializeResult:
+def nyc_manhattan_stats() -> MaterializeResult:
     query = """
         select
             zones.zone,
@@ -32,5 +36,39 @@ def nyc_manhattan_stats(context: AssetExecutionContext) -> MaterializeResult:
     return MaterializeResult(
         metadata={
             "Preview": MetadataValue.md(zone_w_trip_count.head().to_markdown())
+        }
+    )
+
+
+@asset(
+    deps=[nyc_manhattan_stats]
+)
+def nyc_manhattan_map() -> MaterializeResult:
+    # * load gepjson to geopandas dataframe
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+    fig: plotly.graph_objs.Figure = px.choropleth_mapbox(
+        trips_by_zone,
+        geojson=trips_by_zone.geometry.__geo_interface__,
+        locations=trips_by_zone.index,
+        color='num_trips',
+        color_continuous_scale='Plasma',
+        mapbox_style='carto-positron',
+        center={'lat': 40.758, 'lon': -73.985},
+        zoom=11,
+        opacity=0.7,
+        labels={'num_trips': 'Number of Trips'}
+    )
+
+
+    map_img_data = fig.to_image("png")
+    # both approach below work well
+    # pio.write_image(fig, constants.MANHATTAN_MAP_FILE_PATH)
+    # or
+    with open(constants.MANHATTAN_MAP_FILE_PATH, "wb") as f:
+        f.write(map_img_data)
+
+    return MaterializeResult(
+        metadata={
+            "Preview": MetadataValue.md(f"![Manhattan trips status](data:image/png;base64,{base64.b64encode(map_img_data).decode()})")
         }
     )
