2ee2e4eeb9381771b694a40f4aabc838583acb6e
minhnq0702 <minhnq.0702@gmail.com>
2025-01-19 21:39:37 +0700

    fix: format

--- hacker-earth/level_easy/mex.py
+++ hacker-earth/level_easy/mex.py
@@ -1,7 +1,5 @@
-"""
-https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/find-mex-62916c25/
-Find minimal excluded
-"""
+# https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/find-mex-62916c25/
+# Find minimal excluded
 def find_mex():
     """
     Input and find Mex
@@ -10,7 +8,6 @@ def find_mex():
     # lst_str = input()
     with open("./easy/mex.txt", "r", encoding="utf-8") as f:
         arr = f.readlines()
-    n = arr[0]
     lst_str = arr[1]
 
     lst = map(int, lst_str.strip().split(" "))
